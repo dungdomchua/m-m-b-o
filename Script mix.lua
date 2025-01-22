c044ae9aebd99f7a578cf9d505d082a77e77806f
dungdomchua <dungdepzai2013@gmail.com>
2025-01-22 18:42:10 +0700

    Update and rename zingmod.txt to Script mix.lua

---
+++ Script mix.lua
@@ -0,0 +1,155 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/vinh12eqweaws/zingmods/refs/heads/main/zingpro')))()
+
+local Window = OrionLib:MakeWindow({Name = "dungx best skid", HidePremium = false, SaveConfig = true, ConfigFolder = "dxl_bf"})
+
+local Tab = Window:MakeTab({
+	Name = "Scripts mới",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local Tab2 = Window:MakeTab({
+	Name = "Client",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local Tab3 = Window:MakeTab({
+	Name = "buff game",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+local Tab4 = Window:MakeTab({
+	Name = "Script bth",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+Tab3:AddButton({
+	Name = "Fix Lag",
+	Callback = function()
+        repeat wait(5) until game:IsLoaded()
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/TurboLite/Script/main/FixLag.lua"))()
+end
+})
+
+Tab3:AddButton({
+	Name = "j97 hub by thx skid",
+	Callback = function()
+        repeat wait(5) until game:IsLoaded()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/dungdomchua/Bruh/refs/heads/main/oniichan.txt"))()
+end
+})
+
+
+Tab2:AddButton({
+	Name = "Fruit Finder",
+	Callback = function()
+        repeat wait(5) until game:IsLoaded()
+ServerHopTimer = 5
+TeleportSafe = true
+Webhook = "Your Webhook Here"
+_G.Version="Fruit Farm"loadstring(game:HttpGet("https://raw.githubusercontent.com/Efe0626/RaitoHub/main/Script"))()
+  	end    
+})
+
+Tab2:AddButton({
+	Name = "Arceus X",
+	Callback = function()
+        repeat wait(5) until game:IsLoaded()
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/chillz-workshop/main/Arceus%20X%20V3"))()
+end
+})
+
+Tab2:AddButton({
+	Name = "Krnl",
+	Callback = function()
+        repeat wait(5) until game:IsLoaded()
+      loadstring(game:HttpGet("https://pastebin.com/raw/M3HjLPU7"))()
+end
+})
+
+Tab2:AddButton({
+	Name = "Synapse X",
+	Callback = function()
+        repeat wait(5) until game:IsLoaded()
+      loadstring(game:HttpGet("https://pastebin.com/raw/QmqSgQ3K"))()
+end
+})
+
+Tab2:AddButton({
+	Name = "Keyboard",
+	Callback = function()
+        repeat wait(5) until game:IsLoaded()
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/advxzivhsjjdhxhsidifvsh/mobkeyboard/main/main.txt", true))()
+end
+})
+
+Tab:AddButton({
+	Name = "farm chest",
+	Callback = function()
+       _G.Team = "Marine" -- "Marine" or "Pirate"
+_G.MetodeTeleport = "Instant" --"Instant" or "Tween"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ThundarZ/Welcome/refs/heads/main/Main/BloxFruit/Chest/AllDevices.lua"))()
+  	end    
+})
+Tab:AddButton({
+	Name = "andepzai hub",
+	Callback = function()
+repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AnDepZaiHub/AnDepZaiHubBeta/refs/heads/main/AnDepZaiHubNewUpdated.lua"))()
+  	end    
+})
+Tab:AddButton({
+	Name = "strix",
+	Callback = function()
+      loadstring(game:HttpGet"https://scriptroblox.onrender.com/strix")()
+  	end    
+})
+
+Tab:AddButton({
+	Name = "quantum",
+	Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/FlazhGG/QTONYX/refs/heads/main/NextGeneration.lua"))()
+end    
+})
+
+Tab4:AddButton({
+	Name = "Hoho Hub",
+	Callback = function()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/main/Loading_UI'))()
+  	end    
+})
+Tab4:AddButton({
+	Name = "w azure",
+	Callback = function()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b2169cf53bc6104dabe8e19562e5cc2.lua"))()
+  	end    
+})								Tab4:AddButton({
+	Name = "xero",
+	Callback = function()
+getgenv().Team = "Marines"
+getgenv().Hide_Menu = false
+loadstring(game:HttpGet("https://apixerohub.x10.mx/main.lua"))()
+  	end    
+})									Tab4:AddButton({
+	Name = "bap red",
+	Callback = function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaCrack/BapRed/main/BapRedHub"))()
+  	end    
+})								Tab4:AddButton({
+	Name = "min gaming",
+	Callback = function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaCrack/Min/refs/heads/main/MinSeHubVn"))()
+  	end    
+})								Tab4:AddButton({
+	Name = "zis roblox",
+	Callback = function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaCrack/Zis/refs/heads/main/ZisRb7"))()
+  	end    
+})								Tab4:AddButton({
+	Name = "redz",
+	Callback = function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua"))() 
+  	end    
+})											
